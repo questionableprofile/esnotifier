df6dc1006d9ad5d42c12c4408d3b65ea9e4750b2
tldr <40418692+questionableprofile@users.noreply.github.com>
2020-07-06 21:09:42 +0700

    Update Telegram message formatting

--- src/modules/telegram.js
+++ src/modules/telegram.js
@@ -29,18 +29,30 @@ export default class TelegramModule {
         const eventData = data.eventData;
         const actor = data.actor;
 
-        let result = gameData && gameData.node ? `[${gameData.node}]` : '';
+        let result = gameData && gameData.node ? `[${gameData.node}] ` : '';
 
         switch(code) {
             case 'chat':
-                result += ` ${actor.id}:${actor.name}: ${eventData.message}`;
+                result += `[${actor.id}] ${actor.name}: ${eventData.message}`;
+                break;
+            case 'tryMessage':
+                result += `[${actor.id}] ${actor.name}: ${eventData.success ? 'успешно': 'безуспешно'} ${eventData.message}`;
+                break;
+            case 'userRoll':
+                result += `[${actor.id}] [roll] ${actor.name} rolled ${eventData.num}`;
+                break;
+            case 'diceResult':
+                let diceString = '';
+                eventData.rolls.forEach(dice => {
+                    diceString = diceString.concat(`${dice.num}/${dice.sides} `);
+                })
+                result += `[${actor.id}] [dice] ${actor.name} rolled ${diceString}`;
                 break;
             default:
                 console.warn(`unknown event code: ${code}`);
-                result += '[no data]';
+                result += `[no data] [${code}]`;
         }
-
-        this.bot.sendMessage(mconfig.chatId, `${code}`);
+        console.log(`received event '${code}'`);
         this.bot.sendMessage(mconfig.chatId, result);
     }
 
